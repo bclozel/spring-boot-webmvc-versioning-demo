a3967f14c3bd6afbc2aa4f54d888c2d2f4f79c77
Brian Clozel <bclozel@vmware.com>
2020-05-26 14:05:29 +0200

    Use WebMvcRegistrations

---
+++ src/main/java/com/example/demo/CustomWebMvcRegistrations.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+@Component
+public class CustomWebMvcRegistrations implements WebMvcRegistrations {
+
+	@Override
+	public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
+		return new ApiVersionedRequestMapping();
+	}
+}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
-	void contextLoads() {
+	void hasSingleRequestMappingHandlerMappingBean() {
+		assertThat(applicationContext.getBeansOfType(RequestMappingHandlerMapping.class)).hasSize(1);
+		assertThat(applicationContext.getBean(RequestMappingHandlerMapping.class)).isExactlyInstanceOf(ApiVersionedRequestMapping.class);
 	}
 
 }
